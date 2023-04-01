b62b4b9a2a9953c48c2aec133670d06a50fa0757
cynthia <cynthiakagz@gmail.com>
2023-04-01 19:52:04 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(activity= 'roller-skate') {
+   return `This Saturday, I want to ${activity}!`;
+ 
+}
+function mondayWork(activity=`go to the office`) {
+  return `This Monday, I will ${activity}.`;
+   
+}
+function wrapAdjective (string='*') {
+    return function(special='wrapAdjective') {
+       return `You are${string}a hard worker${string}!`;
+    }
+ }
+
+function wrapAdjective (string ='||') {
+return function (special='wrapAdjective') {
+    return `You are ${string}a dedicated programmer${string}!`;
+}
+ 
+    
+}
+
+
+
+
+
+
+
